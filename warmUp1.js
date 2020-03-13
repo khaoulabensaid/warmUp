97ebabe352d185a12abe28d7456037c6f49e7618
khaoulabensaid <bensaidkhaoula59@gmail.com>
2020-03-13 10:36:19 +0100

    done

--- warmUp1.js
+++ warmUp1.js
@@ -1,6 +1,19 @@
 // 1-using + operator combine your partner first and last name .
+'mouna'+' '+'khila';
 // 2-find if the number 13 is a multiple of 3 or not.
+function multiply(n){
+	if (n%3=== 0){
+		return n+' '+'is a mutiple of 3';
+	}
+	return n+' '+' is not multiple of 3';
+}
 // 3-calculate the average age of the follwing ages [13,14,13,15,16,17,19,13,16,15]
+function 
+
 // 4-calculate your age in seconds.
+function age(n){
+	var total= 60*60*24*365*n;
+	return total;
+}
 
 // your code is here
